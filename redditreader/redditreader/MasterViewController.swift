94cefcd22a5e9dbd63aefb1be7e21bb402016669
Ale Mohamad <hello@alemohamad.com>
2018-03-10 03:36:19 -0300

    Deselect selected row.

--- redditreader/redditreader/MasterViewController.swift
+++ redditreader/redditreader/MasterViewController.swift
@@ -86,6 +86,10 @@ class MasterViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     // MARK: - Functions
     
     func readJson() -> RedditList? {
